737a09e6a1628875543160afa0bff7481fec938d
Josh Nixon <seriousfools@gmail.com>
2016-03-17 14:38:20 -0500

    updated time adjustment2

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -5,7 +5,7 @@ class VisitsController < ApplicationController
 
   def create
     url_adjusted = (params["url"].split("/"))[2]
-    time_adjusted = DateTime.strptime(params["time",'%s')
+    time_adjusted = DateTime.strptime(params["time"],'%s')
     Visit.create({url: url_adjusted, email: params["email"], start_time: time_adjusted})
     render json: {message: "success"}
   end
